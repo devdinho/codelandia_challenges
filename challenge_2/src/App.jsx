5917fed7164d77e036fb5a17462bf0b505a3bc3b
Diego Figueirêdo <8kball@gmail.com>
2023-01-07 16:28:09 -0300

    feat: add section with store information

--- challenge_2/src/App.jsx
+++ challenge_2/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/Header';
 import Hero from './components/Hero';
+import InfoSection from './components/InfoSection';
 import Title from './components/Title';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Title />
       <Hero />
+      <InfoSection />
     </div>
   )
 }
---
+++ challenge_2/src/components/InfoSection/index.jsx
@@ -0,0 +1,12 @@
+import "./style.css";
+
+const InfoSection = () => {
+  return (
+    <section className="info-section">
+      <h2>Destaques</h2>
+      <p>Frete grátis e chinelo de brinde é aqui, aproveite por tempo limitado.</p>
+    </section>
+  )
+};
+
+export default InfoSection;
