3db61af001c3986fd045b2421ad12e40ab2d27df
Diego Figueirêdo <8kball@gmail.com>
2023-01-07 15:29:26 -0300

    feat: add promotion header with base style

---
+++ challenge_2/src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const Header = () => {
+  return (
+    <header className="promo-header">
+      Ganhe R$ 10,00 de desconto no frete
+    </header>
+  )
+};
+
+export default Header;
