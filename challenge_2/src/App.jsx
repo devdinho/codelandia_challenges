1312e03c19af2c1c40055d7fcf37a4d894ace4cd
Diego Figueirêdo <8kball@gmail.com>
2023-01-07 15:37:04 -0300

    feat: add title with base style

--- challenge_2/src/App.jsx
+++ challenge_2/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header';
+import Title from './components/Title';
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <Title />
     </div>
   )
 }
---
+++ challenge_2/src/components/Title/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>JordanShoes</h1>
+    </div>
+  )
+};
+
+export default Title;
